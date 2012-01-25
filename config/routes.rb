9b009cf872b8be8ac5ff3408eb969793736f0aea
wwm wwm <zask.mz@gmail.com>
2012-01-25 12:52:15 +0200

    Done with sign in

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 FirstApp::Application.routes.draw do
 
+  resources :sessions, :only => [:new, :create, :destroy]
+  match '/signup',  :to => 'users#new'
+  match '/signin',  :to => 'sessions#new'
+  match '/signout', :to => 'sessions#destroy'
+
   get "users/new"
 
-  match '/signup',  :to => 'users#new'
 
   match '/contact', :to => 'pages#contact'
   match '/about',   :to => 'pages#about'
